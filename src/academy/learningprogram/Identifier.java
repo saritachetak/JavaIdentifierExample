47632b5694b61240eb9c1ba54cb982b12a000169
sarita <sarita.chetak@gmail.com>
2019-08-16 21:13:57 +0100

    aaaaa

---
+++ src/academy/learningprogram/Identifier.java
@@ -0,0 +1,28 @@
+package academy.learningprogram;
+
+public class Identifier {
+    public static void main(String[] args) {
+        int x;
+
+        int 1a;         // won't compile since it starts with number
+        int _1a;        // valid and compiles
+        int aaa;        // valid and compiles
+        int main;       // valid and compiles as this it is
+        int static;
+        int void;
+        int String;
+        int 123456a;
+        int $1234567;
+        int _1234567;
+        int 1234567a;
+
+        //int do;
+        int Do;
+        int dO;
+
+    }
+
+    }
+}
+
+
---
+++ src/academy/learningprogram/learningprogram.java
@@ -0,0 +1,4 @@
+package academy.learningprogram;
+
+public class  {
+}
